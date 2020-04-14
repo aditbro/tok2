95974655682331b3293e1e1d34f12399d1691342
aditbro <adityafarizki@live.com>
2020-04-14 21:46:10 +0700

    temporarily change the pumpdetector

--- src/views/controller/motionDetector/pump.js
+++ src/views/controller/motionDetector/pump.js
@@ -1,68 +1,96 @@
 const pumpDetector = {};
-pumpDetector.currentPower = 0;
-pumpDetector.upThreshold = 10;
-pumpDetector.downThresholds = [16, 18, 20, 22, 25];
-pumpDetector.pumpBuffer = [];
-pumpDetector.pumpUp = function () {
-  let pointMessage = { action: "game_action", action_type: "pump_up" }
-}
 
-pumpDetector.pumpDown = function (power) {
-  let pointMessage = { action: "game_action", action_type: "pump_down", power: power }
-  client.send(JSON.stringify(pumpPayload))
-}
+pumpDetector.upThreshold = 10;
+pumpDetector.downThreshold = 20;
 
 pumpDetector.handleMotion = (event) => {
-  if (event.acceleration.z > pumpDetector.downThresholds[0]) {
+  if (event.acceleration.z > pumpDetector.downThreshold) {
     console.log("down");
-    pumpDetector.pumpBuffer.push(event.acceleration.z);
-
-  } else if (event.acceleration.z < -pumpDetector.upThreshold) {
+    pumpDetector.onPumpUp();
+  } else if(event.acceleration.z < -pumpDetector.upThreshold) {
     console.log("up");
-    pumpDetector.pumpBuffer.push(event.acceleration.z);
+    pumpDetector.onPumpDown();
   }
 };
 
 pumpDetector.start = () => {
   window.addEventListener('devicemotion', pumpDetector.handleMotion);
-  pumpDetector.isRunning = true
-  pumpDetector.handleScoring();
 }
 
 pumpDetector.stop = () => {
   window.removeEventListener('devicemotion', pumpDetector.handleMotion);
-  pumpDetector.isRunning = false
-}
-
-pumpDetector.handleScoring = async function () {
-  while (true && pumpDetector.isRunning) {
-    if (pumpDetector.pumpBuffer.length != 0) {
-      let maxPower = 0;
-      let pumpingDown = false;
-      while (pumpDetector.pumpBuffer.length != 0) {
-        let val = pumpDetector.pumpBuffer.pop();
-
-        if (val < 0 && !pumpingDown) {
-          pumpDetector.pumpUp();
-        } else if (val < 0 && pumpingDown) { // Transition from pumping down to pumping up, need to send the max power
-          pumpDetector.pumpDown(maxPower);
-          pumpDetector.pumpUp();
-          pumpingDown = false;
-          break;
-        } else if (val > 0) {
-          while (val > pumpDetector.downThresholds[maxPower] && maxPower < 4) {
-            maxPower++;
-          }
-          pumpingDown = true;
-        }
-
-        let debug = { z: val, pow: maxPower }
-        client.send(JSON.stringify(debug));
-
-      }
-
-    }
-  }
 }
 
 export default pumpDetector;
+
+/************** THIS METHOD SHOULD BE BETTER BUT NO IDEA HOW TO MAKE IT WORK YET *************/
+
+// const pumpDetector = {};
+// pumpDetector.currentPower = 0;
+// pumpDetector.upThreshold = 10;
+// pumpDetector.downThresholds = [16, 18, 20, 22, 25];
+// pumpDetector.pumpBuffer = [];
+// pumpDetector.pumpUp = function () {
+//   let pointMessage = { action: "game_action", action_type: "pump_up" }
+// }
+
+// pumpDetector.pumpDown = function (power) {
+//   let pointMessage = { action: "game_action", action_type: "pump_down", power: power }
+//   client.send(JSON.stringify(pumpPayload))
+// }
+
+// pumpDetector.handleMotion = (event) => {
+//   if (event.acceleration.z > pumpDetector.downThresholds[0]) {
+//     console.log("down");
+//     pumpDetector.pumpBuffer.push(event.acceleration.z);
+//   } else if (event.acceleration.z < -pumpDetector.upThreshold) {
+//     console.log("up");
+//     pumpDetector.pumpBuffer.push(event.acceleration.z);
+//   }
+
+//   pumpDetector.handleScoring();
+// };
+
+// pumpDetector.start = () => {
+//   window.addEventListener('devicemotion', pumpDetector.handleMotion);
+//   pumpDetector.isRunning = true
+//   pumpDetector.handleScoring();
+// }
+
+// pumpDetector.stop = () => {
+//   window.removeEventListener('devicemotion', pumpDetector.handleMotion);
+//   pumpDetector.isRunning = false
+// }
+
+// pumpDetector.handleScoring = function () {
+//   while (true && pumpDetector.isRunning) {
+//     if (pumpDetector.pumpBuffer.length != 0) {
+//       let maxPower = 0;
+//       let pumpingDown = false;
+//       while (pumpDetector.pumpBuffer.length != 0) {
+//         let val = pumpDetector.pumpBuffer.pop();
+
+//         if (val < 0 && !pumpingDown) {
+//           pumpDetector.pumpUp();
+//         } else if (val < 0 && pumpingDown) { // Transition from pumping down to pumping up, need to send the max power
+//           pumpDetector.pumpDown(maxPower);
+//           pumpDetector.pumpUp();
+//           pumpingDown = false;
+//           break;
+//         } else if (val > 0) {
+//           while (val > pumpDetector.downThresholds[maxPower] && maxPower < 4) {
+//             maxPower++;
+//           }
+//           pumpingDown = true;
+//         }
+
+//         let debug = { z: val, pow: maxPower }
+//         client.send(JSON.stringify(debug));
+
+//       }
+
+//     }
+//   }
+// }
+
+// export default pumpDetector;
